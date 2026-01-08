eacc1a7bb54d928426fb256b7e1ba45f4cabc6d2
chris <chrisnieves60@gmail.com>
2026-01-08 11:12:32 -0500

    syntax error removal

--- backend/src/main/java/com/group1/tipton_reservations/config/SecurityConfig.java
+++ backend/src/main/java/com/group1/tipton_reservations/config/SecurityConfig.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-=======
 import org.springframework.security.access.expression.method.DefaultMethodSecurityExpressionHandler;
 import org.springframework.security.access.expression.method.MethodSecurityExpressionHandler;
 import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
